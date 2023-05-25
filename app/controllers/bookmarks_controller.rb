2584939af6c8d004553c1076a3a6dfea1392861e
Gioia Hauri <gioiahauri@proton.me>
2023-05-25 15:22:59 +0200

    Controllers set up

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,36 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:create, :new]
+  before_action :set_bookmark, only: [:destroy]
+
+  def new
+    @bookmark = @list.bookmarks.build
+  end
+
+  def create
+    @bookmark = @list.bookmarks.build(bookmark_params)
+    if @bookmark.save
+      redirect_to @list, notice: "Bookmark was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_back fallback_location: "/", notice: "Bookmark was successfully destroyed."
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
 end
